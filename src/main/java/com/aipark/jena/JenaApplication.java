c14d2875b561124d26fc8916439ba6d5f3546201
yoongyum <imshreg@naver.com>
2022-09-06 17:41:58 +0900

    conf: first Commit

---
+++ src/main/java/com/aipark/jena/JenaApplication.java
@@ -0,0 +1,13 @@
+package com.aipark.jena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/aipark/jena/domain/Member.java
@@ -0,0 +1,4 @@
+package com.aipark.jena.domain;
+
+public class Member {
+}
