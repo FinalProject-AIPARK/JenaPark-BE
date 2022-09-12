410a42c8d029a948b340f1e8c0b5d6c4400b54cf
yoongyum <imshreg@naver.com>
2022-09-13 03:39:44 +0900

    feat: Update Member  - Issue #9
    나중에 추가로 필요한 필드들을 추가한다.
    (BaseTimeEntity 도 필요함.)

--- src/main/java/com/aipark/jena/domain/Member.java
+++ src/main/java/com/aipark/jena/domain/Member.java
@@ -1,4 +1,39 @@
 package com.aipark.jena.domain;
 
-public class Member {
+import com.aipark.jena.enums.Authority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "MEMBER")
+@Entity
+public class Member{
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String profileImg;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
 }
