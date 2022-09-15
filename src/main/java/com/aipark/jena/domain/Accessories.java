29c1918d304c6e41e6a144990427922c930ba356
Chang1235 <dlckdgml1235@naver.com>
2022-09-15 20:16:16 +0900

    feat: Add Accessories Entity

---
+++ src/main/java/com/aipark/jena/domain/Accessories.java
@@ -0,0 +1,29 @@
+package com.aipark.jena.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "ACESSORIES")
+public class Accessories {
+    @Id
+    @GeneratedValue
+    @Column(name = "accessory_id")
+    private Long id;
+
+    @Column
+    private String accessoryUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "avatar_id")
+    private Avatar avatarId;
+}
