6cbb08d38be08ca7c14f038af2c6550015c1cc5b
Chang1235 <dlckdgml1235@naver.com>
2022-09-15 20:17:21 +0900

    feat: Add Accessories Entity #14

---
+++ src/main/java/com/aipark/jena/domain/Attitude.java
@@ -0,0 +1,29 @@
+package com.aipark.jena.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "ATTITUDE")
+public class Attitude {
+    @Id
+    @GeneratedValue
+    @Column(name = "attitude_id")
+    private Long id;
+
+    @Column
+    private String attitudeUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "avatar_id")
+    private Avatar avatarId;
+}
