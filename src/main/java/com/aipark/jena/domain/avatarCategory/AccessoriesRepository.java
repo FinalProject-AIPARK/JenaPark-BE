9011b28f5a67c12d3405d11de342209d059f8c81
Chang1235 <dlckdgml1235@naver.com>
2022-09-16 19:51:20 +0900

    feat: Add ResponseAvatar Issue #18

--- src/main/java/com/aipark/jena/domain/avatarCategory/AccessoriesRepository.java
+++ src/main/java/com/aipark/jena/domain/avatarCategory/AccessoriesRepository.java
@@ -2,5 +2,10 @@ package com.aipark.jena.domain.avatarCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccessoriesRepository extends JpaRepository<Accessories,Long> {
+    List<Accessories> findAllByAvatarId(Long avatarId);
+
 }
