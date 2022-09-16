3b83a362b548ac5e46e244895805f8dcd98dfe5d
Chang1235 <dlckdgml1235@naver.com>
2022-09-16 19:51:40 +0900

    feat: Add ResponseAvatar Issue #17

---
+++ src/main/java/com/aipark/jena/dto/ResponseAvatar.java
@@ -0,0 +1,20 @@
+package com.aipark.jena.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ResponseAvatar {
+    private String avatarName;
+    private String thumbNail;
+    private List accUrl;
+    private List clothesUrl;
+    private List attitudeUrl;
+}
