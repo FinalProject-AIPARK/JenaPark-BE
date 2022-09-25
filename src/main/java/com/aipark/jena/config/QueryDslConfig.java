c00c3280d6ed6842659310dbe67c1f5e8a16630f
WindowH22 <dlckdgml1235@naver.com>
2022-09-25 23:06:58 +0900

    conf: Add QueryDslConfig #41

---
+++ src/main/java/com/aipark/jena/config/QueryDslConfig.java
@@ -0,0 +1,17 @@
+package com.aipark.jena.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {return new JPAQueryFactory(em);}
+}
