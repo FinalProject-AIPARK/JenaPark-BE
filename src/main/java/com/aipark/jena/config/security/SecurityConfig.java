051103553c1d51d5adf8cd12c84f2e6d66c3d853
soyoungkangme <soyoungkangme@gmail.com>
2022-09-28 21:54:32 +0900

    feat: resotre SecurityConfig - Issue #16

--- src/main/java/com/aipark/jena/config/security/SecurityConfig.java
+++ src/main/java/com/aipark/jena/config/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/members/signup", "/api/v1/members/login", "/token/**").permitAll()
+                .antMatchers("/api/v1/members/signup", "/api/v1/members/login").permitAll()
                 //.anyRequest().authenticated()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .and()
@@ -70,9 +70,7 @@ public class SecurityConfig {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(new JwtExceptionFilter(), OAuth2LoginAuthenticationFilter.class)
-                .oauth2Login().loginPage("/token/expired")
-                .successHandler(successHandler)
+                .oauth2Login()
                 .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
                 .userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
     }
