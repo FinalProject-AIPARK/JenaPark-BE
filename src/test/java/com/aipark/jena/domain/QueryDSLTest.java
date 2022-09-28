128859643ecebadd6239155e0064e33b18cf0450
WindowH22 <dlckdgml1235@naver.com>
2022-09-28 21:33:22 +0900

    chore: Delete not use import Issue #41

--- src/test/java/com/aipark/jena/domain/QueryDSLTest.java
+++ src/test/java/com/aipark/jena/domain/QueryDSLTest.java
@@ -1,14 +1,8 @@
 package com.aipark.jena.domain;
 
-import com.aipark.jena.domain.avatarCategory.QAccessories;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class QueryDSLTest {
