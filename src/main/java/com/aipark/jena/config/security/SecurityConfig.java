70925c1f3e15324af4f60f2cf9d3768363c902e6
soyoungkangme <soyoungkangme@gmail.com>
2022-09-28 22:14:13 +0900

    feat: Edit SecurityConfig - Issue #16

--- src/main/java/com/aipark/jena/config/security/SecurityConfig.java
+++ src/main/java/com/aipark/jena/config/security/SecurityConfig.java
@@ -37,13 +37,14 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/members/signup", "/api/v1/members/login").permitAll()
+                .antMatchers("/api/v1/members/signup", "/api/v1/members/login", "/token/**").permitAll()
                 //.anyRequest().authenticated()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .and()
                 .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class);
+                // .addFilterBefore(new JwtAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -70,7 +71,9 @@ public class SecurityConfig {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .oauth2Login()
+                .addFilterBefore(new JwtExceptionFilter(), OAuth2LoginAuthenticationFilter.class)
+                .oauth2Login().loginPage("/token/expired")
+                .successHandler(successHandler)
                 .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
                 .userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
     }
