94ca37663072aff829dac0bb7fab921d67be1c39
soyoungkangme <soyoungkangme@gmail.com>
2022-09-28 16:05:42 +0900

    feat: Create OAuthService - Issue #16

--- src/main/java/com/aipark/jena/config/security/SecurityConfig.java
+++ src/main/java/com/aipark/jena/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.aipark.jena.config.security;
 
 import com.aipark.jena.config.jwt.JwtAuthenticationFilter;
 import com.aipark.jena.config.jwt.JwtTokenProvider;
+import com.aipark.jena.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
