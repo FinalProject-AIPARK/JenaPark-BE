50eb3ccc3e05c1706a25339ae8a269398fe5cea7
soyoungkangme <soyoungkangme@gmail.com>
2022-09-28 19:15:32 +0900

    feat: Edit OAuthService - Issue #16

--- src/main/java/com/aipark/jena/service/OAuthService.java
+++ src/main/java/com/aipark/jena/service/OAuthService.java
@@ -4,6 +4,7 @@ import com.aipark.jena.domain.Member;
 import com.aipark.jena.domain.MemberRepository;
 import com.aipark.jena.dto.UserProfile;
 import com.aipark.jena.dto.OAuthAttributes;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -11,10 +12,12 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Map;
 
+@Service
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final MemberRepository memberRepository;
 
