eeebc8bdcfb0f376d0c5780176db83a16881ad8c
yoongyum <imshreg@naver.com>
2022-09-29 17:48:23 +0900

    feat: Add project field - Issue #43

--- src/main/java/com/aipark/jena/domain/Project.java
+++ src/main/java/com/aipark/jena/domain/Project.java
@@ -58,6 +58,12 @@ public class Project extends BaseTimeEntity {
     private Boolean audioMerge;     // 오디오 합성여부 미리듣기나 다운로드를 하려면 true
 
     @Column
+    private String audioFileS3Path;
+
+    @Column
+    private String audioFileOriginName;
+
+    @Column
     private String audioFileUrl;   // 전체 오디오 파일 url 미리듣기 등
 
     @Column
@@ -89,6 +95,14 @@ public class Project extends BaseTimeEntity {
 
     public void updateAudioFileUrl(String audioFile) {
         this.audioFileUrl = audioFile;
+    }
+
+    public void updateAudioFileS3Path(String audioFileS3Path) {
+        this.audioFileS3Path = audioFileS3Path;
+    }
+
+    public void updateText(String text) {
+        this.text = text;
     }
 
     public void updateStep1(String allText, String sex, String lang, Double durationSilence, Long volume, Double pitch, Double speed) {
