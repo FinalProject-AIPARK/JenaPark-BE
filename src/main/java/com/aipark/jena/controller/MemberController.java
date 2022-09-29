c51310aeadea0dfcdb69ff51ea57a7deaa41c9c5
yoongyum <imshreg@naver.com>
2022-09-29 17:46:41 +0900

    feat: Add getInfo - Closed #52
    회원 정보 데이터를 반환하는 api 구현

--- src/main/java/com/aipark/jena/controller/MemberController.java
+++ src/main/java/com/aipark/jena/controller/MemberController.java
@@ -1,14 +1,12 @@
 package com.aipark.jena.controller;
 
-import com.aipark.jena.dto.RequestMember;
 import com.aipark.jena.dto.Response.Body;
 import com.aipark.jena.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.aipark.jena.dto.RequestMember.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
@@ -17,22 +15,27 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Body> signUp(@RequestBody RequestMember.SignUp memberDto) {
+    public ResponseEntity<Body> signUp(@RequestBody SignUp memberDto) {
         return memberService.signUp(memberDto);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Body> login(@RequestBody RequestMember.Login loginDto) {
+    public ResponseEntity<Body> login(@RequestBody Login loginDto) {
         return memberService.login(loginDto);
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<Body> reissue(@RequestBody RequestMember.Reissue reissueDto) {
+    public ResponseEntity<Body> reissue(@RequestBody Reissue reissueDto) {
         return memberService.reissue(reissueDto);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Body> logout(@RequestBody RequestMember.Logout logoutDto) {
+    public ResponseEntity<Body> logout(@RequestBody Logout logoutDto) {
         return memberService.logout(logoutDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Body> getInfo() {
+        return memberService.getInfo();
     }
 }
