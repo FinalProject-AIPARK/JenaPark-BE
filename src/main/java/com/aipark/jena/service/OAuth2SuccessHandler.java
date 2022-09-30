051a6cab9df9c1b6eedebd99d3264f9fb3cccbb2
soyoungkangme <soyoungkangme@gmail.com>
2022-09-30 17:38:15 +0900

    feat: Create OAuth2SuccessHandler - Issue #16

---
+++ src/main/java/com/aipark/jena/service/OAuth2SuccessHandler.java
@@ -0,0 +1,44 @@
+package com.aipark.jena.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final TokenService tokenService;
+    private final UserRequestMapper userRequestMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        // Principal에서 OAuth2Use 추출
+        OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+        Dto userDto = userRequestMapper.toDto(oAuth2User);
+
+        // 최초 로그인이면 회원가입 처리
+
+        // Access Token, Refresh Token 생성 및 발급
+        Token token = tokenService.generateToken(userDto.getEmail(), "USER");
+
+        // 토큰 포함하여 리다이렉트
+        String targetUrl;
+        targetUrl = UriComponentsBuilder.fromUriString("/home")
+                .queryParam("token", "token")
+                .build().toUriString();
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
