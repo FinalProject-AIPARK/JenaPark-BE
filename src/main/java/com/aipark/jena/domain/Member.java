ff5d98ba87df871dbf4b3a7af22c60ef066d566f
yoongyum <imshreg@naver.com>
2022-10-06 20:16:21 +0900

    feat: Update Member - Issue #89
    회원 테이블 과 비디오 테이블 양방향 매핑

--- src/main/java/com/aipark/jena/domain/Member.java
+++ src/main/java/com/aipark/jena/domain/Member.java
@@ -18,7 +18,7 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "MEMBER")
 @Entity
-public class Member extends BaseTimeEntity{
+public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
@@ -40,13 +40,24 @@ public class Member extends BaseTimeEntity{
     @OneToMany(mappedBy = "member")
     private List<Project> projects = new ArrayList<>();
 
+    @Builder.Default
+    @OneToMany(mappedBy = "member")
+    private List<Video> videos = new ArrayList<>();
+
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
     public void addProject(Project project) {
         projects.add(project);
-        if(project.getMember() != this) {
+        if (project.getMember() != this) {
             project.setMember(this);
+        }
+    }
+
+    public void addVideo(Video video) {
+        videos.add(video);
+        if (video.getMember() != this) {
+            video.setMember(this);
         }
     }
 }
