5361ba19445898dc4a5eaa276af8455e1b4f63ad
WindowH22 <dlckdgml1235@naver.com>
2022-10-07 20:08:10 +0900

    feat : Add saveOrUpdate Method Issue #16

--- src/main/java/com/aipark/jena/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/aipark/jena/oauth/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.aipark.jena.oauth;
 
-import com.aipark.jena.oauth.OAuth2Attribute;
+import com.aipark.jena.domain.Member;
+import com.aipark.jena.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -14,8 +16,12 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberRepository memberRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         // 1. 로그인 성공정보(code, access token)를 바탕으로 DefaultOAuth2UserService 객체 생성한다.
@@ -32,9 +38,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         // 4. SuccessHandler가 사용할 수 있도록 등록해준다.
         OAuth2Attribute oAuth2Attribute =
                 OAuth2Attribute.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        Member member = saveOrUpdate(oAuth2Attribute);
+
         var memberAttribute = oAuth2Attribute.convertToMap();
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
                 memberAttribute, "email");
+    }
+
+    private Member saveOrUpdate(OAuth2Attribute attribute){
+        Member member = memberRepository.findByEmail(attribute.getEmail())
+                .orElse(attribute.toEntity());
+        return memberRepository.save(member);
     }
 }
