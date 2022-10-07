633e6f532253004d16f3f910cb65e4aebb10f15c
yoongyum <imshreg@naver.com>
2022-10-07 16:28:44 +0900

    test: python test - Issue #54

--- python/createVideo.py
+++ python/createVideo.py
@@ -1,5 +1,5 @@
 import sys
-from s3 import s3_connection, s3_get_object
+from s3 import s3_connection, s3_get_object, s3_put_object
 from media import video_request
 import os
 
@@ -20,8 +20,7 @@ def createVideo():
 
     video_id = video_request(audioId, avatarId, 'result/')
     print(video_id)
-    s3_put_object(s3, "jenapark", f"result/{video_id}.mp4",
-                                        f"video/{video_id}.mp4")
+    s3_put_object(s3, "jenapark", f"result/{video_id}.mp4", f"video/{video_id}.mp4")
     os.remove(f"result/{video_id}.mp4")
     return True
 
