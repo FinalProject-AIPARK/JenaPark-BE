0b4e51b5742667d0e43b6a2f8469b1f1d921c8d1
WindowH22 <dlckdgml1235@naver.com>
2022-10-10 22:15:28 +0900

    chore : Remark token from cookie Issue #16

--- src/main/java/com/aipark/jena/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/aipark/jena/oauth/OAuth2SuccessHandler.java
@@ -36,21 +36,21 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("accessToken = " + tokenRes.getAccessToken());
         log.info("refreshToken = " + tokenRes.getRefreshToken());
 
-        Cookie accessCookie =new Cookie("accessToken",tokenRes.getAccessToken());
-        Cookie refreshCookie =new Cookie("refreshToken",tokenRes.getAccessToken());
+//        Cookie accessCookie =new Cookie("accessToken",tokenRes.getAccessToken());
+//        Cookie refreshCookie =new Cookie("refreshToken",tokenRes.getAccessToken());
 
 //        writeTokenResponse(response, tokenRes);
 //        response.setContentType("text/html;charset=UTF-8");
 
-        response.addCookie(accessCookie);
-        response.addCookie(refreshCookie);
-        log.info("name: "+accessCookie.getName());
-        log.info("value: "+accessCookie.getValue());
+//        response.addCookie(accessCookie);
+//        response.addCookie(refreshCookie);
+//        log.info("name: "+accessCookie.getName());
+//        log.info("value: "+accessCookie.getValue());
 
-//        response.addHeader("Auth", tokenRes.getAccessToken());
-//        response.addHeader("Refresh", tokenRes.getRefreshToken());
+        response.addHeader("Auth", tokenRes.getAccessToken());
+        response.addHeader("Refresh", tokenRes.getRefreshToken());
 
-        response.setContentType("application/json;charset=UTF-8");
+//        response.setContentType("application/json;charset=UTF-8");
 
         getRedirectStrategy().sendRedirect(request,response,"/");
     }
